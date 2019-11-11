453785891bd436e0e7bdaa376bd72c5627a8f0ef
Trece-1101 <omar.bazzi.sf@gmail.com>
2019-11-10 23:17:44 -0300

    terminado
    !!HAY QUE MODIFICAR en GameApplication.csproj la ruta  a donde este la data layer (yo lo tuve que hacer a mano despues de agregar el proyecto data layer)
    linea 15 donde dice Include="C:\Users\Omar\Documents\Unreal Projects\GameApplication\DataLayer\DataLayer.csproj"
    tenes que poner la direccion al proyecto data layer de tu notebook o no va a funcionar nada.

--- GameApplication/Controllers/WHController.cs
+++ GameApplication/Controllers/WHController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DataLayer;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace GameApplication.Controllers
 {
@@ -19,21 +20,66 @@ namespace GameApplication.Controllers
         }
 
 
-        // GET api/values
-        //[HttpGet]
-        //public ActionResult<IEnumerable<string>> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
+        // Este metodo es solo para testear que se llega a la API
+        // En el juego es el metodo que se llama desde "Check Conn"
+        // Aun despues de esto puede haber error en la conexion a la base de datos
+        // Eso se contempla en el C++
+        [HttpGet]
+        [Route("test")]
+        public IActionResult Test() {
+            return Ok("Conexion a la api establecida");
+        }
 
-        // GET api/values/5
-        //[HttpGet("{id}")]
-        //public ActionResult<string> Get(int id)
-        //{
-        //    return "value";
-        //}
 
-        // GET api/values/5
+        // Este metodo guarda una jugada y la suma (1) si el jugador ya existe
+        // Si no existe lo crea (esa implementacion esta en la capa datos)
+        // Es un metodo GET y se le pasa el nombre del jugador por ruta
+        // Por ejemplo '/api/WH/guardar/CosmeFulanito' se fija si existe CosmeFulanito
+        // si no existe lo crea e inicializa sus jugadas en 1, de ya existir le suma a sus jugadas 1
+        [HttpGet]
+        [Route("guardar/{playerName}")]
+        public async void SavePlay([FromRoute] string playerName) {
+            var name = playerName;
+            await gameRepository.GuardarAsync(playerName);
+        }
+
+        // Este metodo es una sobrecarga del anterior, donde se pasa por ruta el nombre del jugador
+        // y un numero X de jugadas, no se sumariza, sobreescribe el numero anterior
+        // este metodo NO sera implementado en el juego, solo creamos el controlador a efectos academicos
+        [HttpGet]
+        [Route("guardarjugadas/{playerName}/{plays}")]
+        public async void SavePlays([FromRoute] string playerName, string plays) {
+            var intPlays = Convert.ToInt32(plays);
+            await gameRepository.GuardarAsync(playerName, intPlays);
+
+        }
+
+        // Sobrecarga del metodo, guarda partida en jugador default
+        // NO se implementa en el juego
+        [HttpGet]
+        [Route("guardardefault")]
+        public async void SaveDefaut() {
+            await gameRepository.GuardarAsync();
+
+        }
+
+        // Sobrecarga del metodo, guarda partidas (sobreescribe) en jugador default
+        // NO se implementa en el juego
+        [HttpGet]
+        [Route("guardardefault/{plays}")]
+        public async void SaveDefaut(string plays) {
+            var intPlays = Convert.ToInt32(plays);
+
+            await gameRepository.GuardarAsync();
+
+        }
+
+        
+
+
+        // Metodo que toma la lista de jugadores almacenados en la base de datos
+       
+
         [HttpGet]
         [Route("players")]
         public async Task<IActionResult> GetPlayers()
@@ -41,41 +87,71 @@ namespace GameApplication.Controllers
             try
             {
                 var jugadores = await gameRepository.ObtenerJugadores();
-                Dictionary<string, string> players = new Dictionary<string, string>();
+                //Dictionary<string, string> players = new Dictionary<string, string>();
 
-                foreach (var jugador in jugadores)
-                {
-                    players.Add(jugador.Nickname, jugador.CantidadJugadas.ToString());
-                }
+                //foreach (var jugador in jugadores) {
+                //    players.Add(jugador.Nickname, jugador.CantidadJugadas.ToString());
+                //}
+
 
                 //var jsonPlayers = JsonConvert.SerializeObject(players);
 
-                return Ok(players);
+                //var jsonPlayers = jugadores.First();
+
+                var jsonPlayers = JsonConvert.SerializeObject(jugadores);                
+
+                return Ok(jsonPlayers);
             }
             catch(Exception ex)
             {
                 return BadRequest(ex);
+            }            
+        }
+
+
+        [HttpGet]
+        [Route("playersByIndex/{index}")]
+        public async Task<IActionResult> GetPlayersByIndex([FromRoute] string index) {
+            try {
+                int indexInt = Convert.ToInt32(index);
+                var jugadores = await gameRepository.ObtenerJugadores();
+
+                var jugador = jugadores[indexInt];
+
+                var jsonNumber = JsonConvert.SerializeObject(jugador);
+
+                return Ok(jsonNumber);
             }
-
-            
+            catch (Exception ex) {
+                return BadRequest(ex);
+            }
         }
+
+
+
 
         [HttpGet]
-        [Route("guardar/{playerName}")]
-        public async void SavePlay([FromRoute] string playerName)
-        {
-            var name = playerName;
-            await gameRepository.GuardarAsync(playerName);
+        [Route("number")]
+        public async Task<IActionResult> GetNumberOfPlayers() {
+            try {
+                var jugadores = await gameRepository.ObtenerJugadores();
+
+                Int32 number = jugadores.Count();
+
+                var jsonNumber = JsonConvert.SerializeObject(number);
+               
+                return Ok(jsonNumber);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex);
+            }
         }
 
-        [HttpGet]
-        [Route("guardarjugadas/{playerName}/{plays}")]
-        public async void SavePlays([FromRoute] string playerName, string plays)
-        {
-            var intPlays = Convert.ToInt32(plays);
-            await gameRepository.GuardarAsync(playerName, intPlays);
 
-        }
+
+
+
+
 
         //// POST api/values
         //[HttpPost]
@@ -95,11 +171,18 @@ namespace GameApplication.Controllers
         //{
         //}
 
-        [HttpGet]
-        [Route("Test")]
-        public IActionResult Test()
-        {
-            return Ok("funciona");
-        }
+        // GET api/values
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/values/5
+        //[HttpGet("{id}")]
+        //public ActionResult<string> Get(int id)
+        //{
+        //    return "value";
+        //}
     }
 }
